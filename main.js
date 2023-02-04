55b1aa759bf5c5499a9871317a44b284646d35e8
Frankin Bermudez <frankinlopez24@gmail.com>
2023-02-04 17:08:27 -0500

    Fusion del menu en mobile (clase 8)

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+//Selector para Boton de la cuenta y el menu de cuenta en desktop
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu  = document.querySelector('.desktop-menu');
 
+//Selector para Boton del menu y el mobile-menu
+const botonMenuMobile = document.querySelector('.menu');
+const MenuMobile = document.querySelector('.mobile-menu');
 
+
+botonMenuMobile.addEventListener('click', quitarMostrarMobileMenu);
 menuEmail.addEventListener('click', quitarMostrarDestoktopMenu);
 
-function quitarMostrarDestoktopMenu(){
-desktopMenu.classList.toggle('inactive')
 
+
+function quitarMostrarDestoktopMenu(){
+    desktopMenu.classList.toggle('ocultar')
+}
+
+function quitarMostrarMobileMenu(){
+    MenuMobile.classList.toggle('ocultar')
 }
